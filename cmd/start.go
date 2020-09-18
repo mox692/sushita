3d874c4cc077ec4a78b4fc6430e33b3264bfbff2
mox692 <55653825+mox692@users.noreply.github.com>
2020-09-18 16:05:30 +0900

    first commit

---
+++ cmd/start.go
@@ -0,0 +1,103 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		startSushita()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+func startSushita() {
+	fmt.Println("↓enter eny command")
+	score := 0
+	timer := time.NewTimer(time.Second * 15)
+	question := []string{"きょうのばんごはん", "かのじょのゆくえ", "あしたのてんき"}
+	s := bufio.NewScanner(os.Stdin)
+	now_question := question[rand.Intn(3)]
+	fmt.Println(now_question)
+
+	remainTime := 15
+	go func() {
+		ad := &remainTime
+		for range time.Tick(1 * time.Second) {
+			*ad = *ad - 1
+		}
+
+	}()
+
+	go func() {
+		for s.Scan() {
+			if s.Text() == now_question {
+				score++
+				fmt.Printf("**********************************\n")
+				fmt.Printf("collect!!\nTime Remain : %d\nScore : %d\n", remainTime, score)
+				fmt.Printf("**********************************\n")
+				now_question = question[rand.Intn(2)]
+				fmt.Println(now_question)
+			} else {
+				fmt.Printf("incollect...\nTime Remain : %d\n\n", remainTime)
+				fmt.Printf("%s\n", now_question)
+			}
+		}
+		<-timer.C
+	}()
+
+	time.Sleep(time.Second * 15)
+	fmt.Printf("\n\n")
+	fmt.Printf("======================\n")
+	fmt.Printf("time over!!\n your score : %d\n", score)
+	fmt.Printf("======================\n")
+
+	if s.Err() != nil {
+		// non-EOF error.
+		log.Fatal(s.Err())
+	}
+
+}
